b595985740d1f294f1ead82aaf40ee2fe7d2a844
Omar Santiago Vilorio Pichardo <ovilorio@caei.com>
2026-03-17 10:09:48 -0400

    Pequeña eliminacion de link en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar({ onLogout, setView }) {
         { name: "Escenarios", view: "scenarios" },
         { name: "Metas de ahorro", view: "goals" },
         { name: "Presupuestos", view: "budgets" },
-        { name: "Dashh", view: "moderndashboard" },
+        // { name: "Dashh", view: "moderndashboard" },
       ],
     },
     {
